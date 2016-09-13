fd3079693cf62c37d9ce57072e5cc05fcac91b25
xuanbka1 <xuanbka1@gmail.com>
2016-09-13 21:47:19 +0700

    full project before edit makefile to build

---
+++ ADC_USART1/src/main.c
@@ -0,0 +1,131 @@
+/*********************************************************************************************************
+ *
+ * File                : main.c
+ * Hardware Environment:
+ * Build Environment   : RealView MDK-ARM  Version: 4.20
+ * Version             : V1.0
+ * By                  :
+ *
+ *                                  (c) Copyright 2005-2011, WaveShare
+ *                                       http://www.waveshare.net
+ *                                          All Rights Reserved
+ *
+ *********************************************************************************************************/
+/*int data = rand() % 1000;
+ char st[4];
+ sprintf(st, "%d", data);
+ USART_SendText(USART1, st);
+ USART_SendText(USART1, "\n\r");
+ */
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f10x.h"
+#include "Usart.h"
+//#include "ADC.h"
+#include <stdio.h>
+#include "SysTick.h"
+#include "string.h"
+
+#define ADC1_DR_Address    ((u32)0x4001244C)
+volatile uint16_t ADCValue;
+/* Private function prototypes -----------------------------------------------*/
+void ADC_Configuration();
+void USART_Configuration(void);
+
+double AD_value;
+vu16 ADC_ConvertedValue;
+/*--------------Main---------------------------------------------------------*/
+int main(void) {
+
+	SysTick_Configuration();
+	USART_Configuration();
+	ADC_Configuration();
+
+	USART_SendText(USART1," Chuong trinh test ADC");
+
+	while (1) {
+
+		 ADCValue = read_adc( ADC1,ADC_Channel_6);
+		char st[10];
+		//AD_value = ADC_ConvertedValue;
+		//AD_value = (AD_value / 4096) * 3.3;
+
+		sprintf(st, "%d", ADCValue);
+		USART_SendText(USART1, st);
+		 USART_SendText(USART1, "\n\r");
+
+
+		SysTick_DelayMs(1000);
+
+	}
+}
+
+void ADC_Configuration(void)
+{
+  ADC_InitTypeDef ADC_InitStructure;
+  DMA_InitTypeDef DMA_InitStructure;
+  GPIO_InitTypeDef GPIO_InitStructure;
+
+  RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1 | RCC_APB2Periph_GPIOA | RCC_APB2Periph_AFIO, ENABLE);
+
+  /* Configure PA.06 (ADC Channel6), PA.07 (ADC Channel7) as analog input -------------------------*/
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7;
+  //GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AIN;
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+  /* DMA channel1 configuration ----------------------------------------------*/
+  DMA_DeInit(DMA1_Channel1);
+  DMA_InitStructure.DMA_PeripheralBaseAddr = ADC1_DR_Address;
+  DMA_InitStructure.DMA_MemoryBaseAddr = (u32)&ADC_ConvertedValue;
+  DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralSRC;
+  DMA_InitStructure.DMA_BufferSize = 1;
+  DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+  DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Disable;
+  DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord;
+  DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_HalfWord;
+  DMA_InitStructure.DMA_Mode = DMA_Mode_Circular;
+  DMA_InitStructure.DMA_Priority = DMA_Priority_High;
+  DMA_InitStructure.DMA_M2M = DMA_M2M_Disable;
+  DMA_Init(DMA1_Channel1, &DMA_InitStructure);
+
+  /* Enable DMA1 channel1 */
+  DMA_Cmd(DMA1_Channel1, ENABLE);
+
+  /* ADC1 configuration ------------------------------------------------------*/
+  ADC_InitStructure.ADC_Mode = ADC_Mode_Independent;
+  ADC_InitStructure.ADC_ScanConvMode = ENABLE;
+  ADC_InitStructure.ADC_ContinuousConvMode = ENABLE;
+  ADC_InitStructure.ADC_ExternalTrigConv = ADC_ExternalTrigConv_None;
+  ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
+  ADC_InitStructure.ADC_NbrOfChannel = 1;
+  ADC_Init(ADC1, &ADC_InitStructure);
+
+  /* ADC1 regular channel6 configuration */
+  ADC_RegularChannelConfig(ADC1, ADC_Channel_6, 1, ADC_SampleTime_239Cycles5);
+
+  /* Enable ADC1 DMA */
+  ADC_DMACmd(ADC1, ENABLE);
+
+  /* Enable ADC1 */
+  ADC_Cmd(ADC1, ENABLE);
+
+  /* Enable ADC1 reset calibaration register */
+  ADC_ResetCalibration(ADC1);
+  /* Check the end of ADC1 reset calibration register */
+  while(ADC_GetResetCalibrationStatus(ADC1));
+
+  /* Start ADC1 calibaration */
+  ADC_StartCalibration(ADC1);
+  /* Check the end of ADC1 calibration */
+  while(ADC_GetCalibrationStatus(ADC1));
+
+  /* Start ADC1 Software Conversion */
+  ADC_SoftwareStartConvCmd(ADC1, ENABLE);
+}
+
+
+
+/*********************************************************************************************************
+      END FILE
+*********************************************************************************************************/
